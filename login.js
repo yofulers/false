18085807da8fbf121993bae0ec20e16bc882110d
yofulers <abdozzino@gmail.com>
2024-08-19 22:12:47 +0100

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function login() {
           var username = document.getElementById("username").value;
           
  
-          if (username === "HEXIOS") {
+          if (username === "NON") {
             // Lưu tên đăng nhập và mật khẩu vào Local Storage
            // localStorage.setItem("username", username);
            // localStorage.setItem("password", password);
